511873f037ee9634cd0563532f8e5f26a3c63559
Stephane Torres <stephane.torres@madetech.com>
2024-03-19 14:12:07 +0000

    pass test for 3 teaspoons equals 1 tablespoon

--- measure.js
+++ measure.js
@@ -5,8 +5,13 @@ class Measure{
 
     }
     equals(measure){
+        if (this.unit === "tsp" && measure.unit === "tbsp") {
+            return (this.quanity / 3) == measure.quanity;
+        } 
+        if (this.unit === "tbsp" && measure.unit === "tsp") {
+            return (this.quanity * 3) == measure.quanity;
+        }
         return this.quanity === measure.quanity && this.unit === measure.unit
-        
     }
 }
 
---
+++ teaspoon.js
@@ -0,0 +1,5 @@
+class Teaspoon {
+
+}
+
+module.exports = Teaspoon;
