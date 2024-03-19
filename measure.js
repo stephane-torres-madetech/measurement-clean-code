12086a67fff5ae77086f3a9571fa9db3e3a5f81f
Stephane Torres <stephane.torres@madetech.com>
2024-03-19 16:07:10 +0000

    massive refactor, quantity pattern

--- measure.js
+++ measure.js
@@ -1,18 +1,33 @@
+class Unit{
+    constructor(relativeRatio, relativeUnit){
+        if (relativeUnit){
+            this._ratioToBaseUnit = relativeRatio * relativeUnit._ratioToBaseUnit;
+        } else {
+            this._ratioToBaseUnit = 1;
+        }
+    }
+
+    s(amount){
+        return new Measure(amount, this);
+    }
+}
+
+const TEASPOON = new Unit()
+const TABLESPOON = new Unit(3, TEASPOON)
+const OUNCE = new Unit(2, TABLESPOON)
+const CUP = new Unit(8, OUNCE)
+
 class Measure{
     constructor(quanity, unit){
         this.quanity = quanity
         this.unit = unit
-
     }
-    equals(measure){
-        if (this.unit === "tsp" && measure.unit === "tbsp") {
-            return (this.quanity / 3) == measure.quanity;
-        } 
-        if (this.unit === "tbsp" && measure.unit === "tsp") {
-            return (this.quanity * 3) == measure.quanity;
-        }
-        return this.quanity === measure.quanity && this.unit === measure.unit
+
+    equals(other){
+        const firstComparison = this.unit._ratioToBaseUnit * this.quanity
+        const secondComparison = other.unit._ratioToBaseUnit * other.quanity
+        return firstComparison === secondComparison
     }
 }
 
-module.exports = Measure
+module.exports = {Measure, TEASPOON, TABLESPOON, OUNCE, CUP}
--- measurment.test.js
+++ measurment.test.js
@@ -1,27 +1,34 @@
-const Measure = require("./measure")
+const {Measure, TEASPOON, TABLESPOON, OUNCE, CUP} = require("./measure")
 
 test("Test one teaspoon is a Teaspoon", () => {
-    const teaspoon = new Measure(1, 'tsp');
+    const teaspoon = new Measure(1, TEASPOON);
     expect(teaspoon.equals(teaspoon)).toBe(true)
 })
 
 test("teaspoons with different quantities are not equal", () => {
-    const teaspoon = new Measure(1, 'tsp');
-    const twoTeaspoons = new Measure(2, 'tsp');
+    const teaspoon = new Measure(1, TEASPOON);
+    const twoTeaspoons = new Measure(2, TEASPOON);
     expect(twoTeaspoons.equals(teaspoon)).toBe(false)
     expect(teaspoon.equals(twoTeaspoons)).toBe(false)
 })
 
 test("test different units are not equal", () => {
-    const teaspoon = new Measure(1, 'tsp');
-    const tablespoon = new Measure(1, 'tbsp');
+    const teaspoon = new Measure(1, TEASPOON);
+    const tablespoon = new Measure(1, TABLESPOON);
     expect(teaspoon.equals(tablespoon)).toBe(false);
     expect(tablespoon.equals(teaspoon)).toBe(false);
 })
 
 test("test 3 teaspoons are equal to a tablespoon", () => {
-    const threeTeaspoons = new Measure(3, 'tsp');
-    const tablespoon = new Measure(1, 'tbsp');
+    const threeTeaspoons = TEASPOON.s(3)
+    const tablespoon = TABLESPOON.s(1);
     expect(threeTeaspoons.equals(tablespoon)).toBe(true)
     expect(tablespoon.equals(threeTeaspoons)).toBe(true)
 })
+
+test('ounces and tablespoons can be compared', () => {
+    expect(OUNCE.s(1).equals(TABLESPOON.s(2))).toBe(true)
+    expect(CUP.s(1).equals(OUNCE.s(8))).toBe(true)
+    expect(CUP.s(1).equals(TABLESPOON.s(16))).toBe(true)
+})
+
